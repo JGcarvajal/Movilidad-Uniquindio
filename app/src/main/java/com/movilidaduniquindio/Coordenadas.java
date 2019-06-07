dc537220482e2fb870770210377ef6f7d1268712
Juan Gabriel Carvajal Hernandez <jgcarvajalh@uqvirtual.edu.co>
2019-06-07 13:38:46 -0500

    Cambios en coordenadas, prueba de despliegue

--- app/src/main/java/com/movilidaduniquindio/Coordenadas.java
+++ app/src/main/java/com/movilidaduniquindio/Coordenadas.java
@@ -122,7 +122,12 @@ Preference preference;
         try {
             double lat;
             double log;
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED
+            ) {
 
 
             } else {
